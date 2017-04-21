40a0a363aa7dceb08eb7cc27b70932b8bd306d75
Makrela <drdak.dominik@gmail.com>
2017-04-21 20:44:31 +0200

    Added function parsing arguments

--- trace.cpp
+++ trace.cpp
@@ -1,4 +1,63 @@
-//
-// Created by makrela on 21.4.2017.
-//
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/ip_icmp.h>
+#include <linux/errqueue.h>
 
+using namespace std;
+
+int first_ttl   = 1;
+int max_ttl     = 30;
+string address;
+
+void parser(int argc, char *argv[]) {
+
+    int option = 0;
+    while (optind < argc) {
+
+        if ((option = getopt(argc, argv, "f:m:")) != -1) {
+
+            switch (option) {
+
+                case 'f': {
+
+                    first_ttl = (int)atol(optarg);
+                    break;
+                }
+
+                case 'm': {
+
+                    max_ttl = (int)atol(optarg);
+                    break;
+                }
+
+                default: {
+                    break;
+                }
+            }
+        }
+
+        else {
+
+            address = argv[optind];
+            optind++;
+        }
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+
+    parser(argc, argv);
+}
